be64d1240a1d13ac0a5d1d52612a7db3364c20c9
Justas Grinaveckis <grinaveckisjustas@gmail.com>
2023-04-04 22:06:15 +0300

    adding views

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GreetingController;
-use App\Http\Controllers\CalculatorController;
-use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +12,3 @@ use App\Http\Controllers\TaskController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return 'Hello World';
-});
-
-Route::get('/{name}/', [GreetingController::class, 'greet']);
-
-Route::get('/{type}/{first}/{second}', [CalculatorController::class, 'calculate']
-)->where(['type' => '[a-z]+', 'first' => '[0-9]+', 'second' => '[0-9]+']);
-
-Route::get('/tasks', [TaskController::class, 'list']);
-Route::post('/tasks', [TaskController::class, 'create']);
-Route::get('/tasks/{id}', [TaskController::class, 'find'])->whereNumber('id');
-Route::post('/tasks/{id}', [TaskController::class, 'update']);
