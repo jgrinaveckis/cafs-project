2786ee38a6543de881645f969572e83481121b2d
Justas Grinaveckis <grinaveckisjustas@gmail.com>
2023-05-09 17:16:06 +0300

    typo in registration

--- node/vue-project/src/views/AboutView.vue
+++ node/vue-project/src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { roles } from '@/composables/roles';
 import Navigation from '../components/Navigation.vue'
 
+const { isAdmin } = roles()
 </script>
 
 <template>
@@ -12,10 +14,12 @@ import Navigation from '../components/Navigation.vue'
             If you want to see live orders go to <router-link class="nav-link display-inline" to="/">Home</router-link>
         </p>
         <p>
-            If you want to see order aggregations go to <router-link class="nav-link display-inline" to="/aggregations">Aggregations</router-link>
+            If you want to see order aggregations go to <router-link class="nav-link display-inline"
+                to="/aggregations">Aggregations</router-link>
         </p>
-        <p>
-            If you want to see currently logged in person info press <router-link class="nav-link display-inline" to="/person"><a>here</a></router-link>
+        <p v-if="isAdmin">
+            If you want to see currently logged in person info press <router-link class="nav-link display-inline"
+                to="/person"><a>here</a></router-link>
         </p>
         <p>
             Source code can be seen in <a href="https://github.com/jgrinaveckis/cafs-project">in Github repo here</a>
--- node/vue-project/src/views/Register/RegisterForm.vue
+++ node/vue-project/src/views/Register/RegisterForm.vue
@@ -7,7 +7,7 @@ const regForm = reactive({
     email: "",
     name: "",
     password: "",
-    confirmPassword: ""
+    password_confirmation: ""
 })
 
 const errors = ref({})
@@ -35,94 +35,73 @@ onMounted(() => emailElement.value?.focus())
 
 <template>
     <section class="vh-100 gradient-custom">
-    <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card bg-dark text-white">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white">
 
-            <div class="card-body p-5 text-center">
+                        <div class="card-body p-5 text-center">
 
-                <div class="mb-md-5 mt-md-4 pb-5">
+                            <div class="mb-md-5 mt-md-4 pb-5">
 
-                    <h2 class="fw-bold mb-2 text-uppercase">Register</h2>
+                                <h2 class="fw-bold mb-2 text-uppercase">Register</h2>
 
-                    <div class="form-outline form-white mb-4">
-                        <label class="form-label" for="username">Username</label>
-                        <input 
-                            type="username" 
-                            id="username" 
-                            class="form-control form-control-lg"
-                            v-model.trim="regForm.name"
-                            ref="username"
-                             />
-                        <p></p>
-                        <div class="form-text text-danger" v-if="errors.name">
-                            {{ errors.name.join(' ') }}
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="username">Username</label>
+                                    <input type="username" id="username" class="form-control form-control-lg"
+                                        v-model.trim="regForm.name" ref="username" />
+                                    <p></p>
+                                    <div class="form-text text-danger" v-if="errors.name">
+                                        {{ errors.name.join(' ') }}
+                                    </div>
+
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="email">Email</label>
+                                    <input type="email" id="email" class="form-control form-control-lg"
+                                        v-model.trim="regForm.email" ref="emailElement" />
+                                    <p></p>
+                                    <div class="form-text text-danger" v-if="errors.email">
+                                        {{ errors.email.join(' ') }}
+                                    </div>
+
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="password">Password</label>
+                                    <input type="password" id="password" class="form-control form-control-lg"
+                                        v-model.trim="regForm.password" />
+                                    <p class="text-white-50">Password have to be longer than 5 symbols</p>
+                                    <div class="form-text text-danger" v-if="errors.password">
+                                        {{ errors.password.join(' ') }}
+                                    </div>
+
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="password_confirmation">Confirm Password</label>
+                                    <input type="password" id="password_confirmation" class="form-control form-control-lg"
+                                        v-model.trim="regForm.password_confirmation" />
+                                    <div class="form-text text-danger" v-if="errors.password_confirmation">
+                                        {{ errors.password_confirmation.join(' ') }}
+                                    </div>
+
+                                </div>
+
+                                <button class="btn btn-outline-light btn-lg px-5" @click="submit" :disabled="isBusy">
+                                    <div class="spinner-border spinner-border-sm" v-if="isBusy"></div>
+                                    Register
+                                </button>
+
+                            </div>
+
                         </div>
-
                     </div>
-
-                    <div class="form-outline form-white mb-4">
-                        <label class="form-label" for="email">Email</label>
-                        <input 
-                            type="email" 
-                            id="email" 
-                            class="form-control form-control-lg"
-                            v-model.trim="regForm.email"
-                            ref="emailElement"
-                             />
-                        <p></p>
-                        <div class="form-text text-danger" v-if="errors.email">
-                            {{ errors.email.join(' ') }}
-                        </div>
-
-                    </div>
-
-                    <div class="form-outline form-white mb-4">
-                        <label class="form-label" for="password">Password</label>
-                        <input 
-                            type="password"
-                            id="password"
-                            class="form-control form-control-lg"
-                            v-model.trim="regForm.password" />
-                        <p class="text-white-50">Password have to be longer than 5 symbols</p>
-                        <div class="form-text text-danger" v-if="errors.password">
-                            {{ errors.password.join(' ') }}
-                        </div>
-
-                    </div>
-
-                    <div class="form-outline form-white mb-4">
-                        <label class="form-label" for="confirmPassword">Confirm Password</label>
-                        <input 
-                            type="password"
-                            id="confirmPassword"
-                            class="form-control form-control-lg"
-                            v-model.trim="regForm.confirmPassword" />
-                        <div class="form-text text-danger" v-if="errors.password_confirmation">
-                            {{ errors.password_confirmation.join(' ') }}
-                        </div>
-
-                    </div>
-
-                    <button 
-                        class="btn btn-outline-light btn-lg px-5" 
-                        @click="submit"
-                        :disabled="isBusy"
-                    >
-                    <div class="spinner-border spinner-border-sm" v-if="isBusy"></div>
-                        Register
-                    </button>
-
                 </div>
-
-            </div>
             </div>
         </div>
-        </div>
-    </div>
     </section>
 </template>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
